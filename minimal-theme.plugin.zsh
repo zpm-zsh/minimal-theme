758e59a5702be7ad806acdcc2709f51bb1a21e15
Grigorii Horos <horosgrisa@gmail.com>
2019-04-17 00:17:41 +0300

    Initial commit

--- minimal-theme.plugin.zsh
+++ minimal-theme.plugin.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+zpm zpm-zsh/pr-is-root zpm-zsh/pr-user 2>/dev/null # PROMPT 
+zpm zpm-zsh/pr-git zpm-zsh/pr-cwd 2>/dev/null # RPROMPT
+zpm zpm-zsh/pr-2 2>/dev/null # PROMPT2
+zpm zpm-zsh/pr-eol 2>/dev/null # PROMPT_EOL_MARK
+zpm zpm-zsh/title 2>/dev/null # PROMPT_TITLE
+
 PROMPT='$pr_is_root$pr_user '
 RPROMPT='$pr_node$pr_git$pr_cwd'
 PROMPT2='$pr_2'
