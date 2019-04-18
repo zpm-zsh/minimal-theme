e85f75c147c8bce6ef6e100e4ba156cd5e40703e
Grigorii Horos <horosgrisa@gmail.com>
2019-04-18 17:45:31 +0300

    Fix README

--- minimal-theme.plugin.zsh
+++ minimal-theme.plugin.zsh
@@ -1,13 +1,19 @@
 #!/usr/bin/env zsh
 
-zpm zpm-zsh/pr-is-root zpm-zsh/pr-user 2>/dev/null # PROMPT 
-zpm zpm-zsh/pr-git zpm-zsh/pr-cwd 2>/dev/null # RPROMPT
+DEPENDENCES_ZSH+=( zpm-zsh/pr-is-root zpm-zsh/pr-jobs zpm-zsh/pr-user )
+DEPENDENCES_ZSH+=( zpm-zsh/pr-exec-time zpm-zsh/pr-git zpm-zsh/pr-cwd )
+DEPENDENCES_ZSH+=( zpm-zsh/pr-2 )
+DEPENDENCES_ZSH+=( zpm-zsh/pr-eol )
+DEPENDENCES_ZSH+=( zpm-zsh/title )
+
+zpm zpm-zsh/pr-is-root zpm-zsh/pr-jobs zpm-zsh/pr-user 2>/dev/null # PROMPT 
+zpm zpm-zsh/pr-exec-time zpm-zsh/pr-git zpm-zsh/pr-cwd 2>/dev/null # RPROMPT
 zpm zpm-zsh/pr-2 2>/dev/null # PROMPT2
 zpm zpm-zsh/pr-eol 2>/dev/null # PROMPT_EOL_MARK
 zpm zpm-zsh/title 2>/dev/null # PROMPT_TITLE
 
-PROMPT='$pr_is_root$pr_user '
-RPROMPT='$pr_node$pr_git$pr_cwd'
+PROMPT='$pr_is_root$pr_jobs$pr_user '
+RPROMPT='$pr_exec_time$pr_node$pr_git$pr_cwd'
 PROMPT2='$pr_2'
 PROMPT_EOL_MARK='$pr_eol'
 PROMPT_TITLE='$USER@$HOST:$PWD'
