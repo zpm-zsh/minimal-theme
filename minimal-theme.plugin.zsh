4e999ccac0004dc8649b93b1b9ea0828d830f4df
Grigorii Horos <horosgrisa@gmail.com>
2019-04-19 00:15:02 +0300

    Fix instruction

--- minimal-theme.plugin.zsh
+++ minimal-theme.plugin.zsh
@@ -7,11 +7,11 @@ DEPENDENCES_ZSH+=( zpm-zsh/pr-eol )
 DEPENDENCES_ZSH+=( zpm-zsh/title )
 
 if which zpm >/dev/null; then
-zpm zpm-zsh/pr-is-root zpm-zsh/pr-jobs zpm-zsh/pr-user # PROMPT 
-zpm zpm-zsh/pr-exec-time zpm-zsh/pr-git zpm-zsh/pr-cwd # RPROMPT
-zpm zpm-zsh/pr-2 # PROMPT2
-zpm zpm-zsh/pr-eol # PROMPT_EOL_MARK
-zpm zpm-zsh/title # PROMPT_TITLE
+  zpm zpm-zsh/pr-is-root zpm-zsh/pr-jobs zpm-zsh/pr-user # PROMPT 
+  zpm zpm-zsh/pr-exec-time zpm-zsh/pr-git zpm-zsh/pr-cwd # RPROMPT
+  zpm zpm-zsh/pr-2 # PROMPT2
+  zpm zpm-zsh/pr-eol # PROMPT_EOL_MARK
+  zpm zpm-zsh/title # PROMPT_TITLE
 fi
 
 PROMPT='$pr_is_root$pr_jobs$pr_user '
